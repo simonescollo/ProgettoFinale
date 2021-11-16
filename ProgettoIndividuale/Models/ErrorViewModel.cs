c6548b3cbead034a14cdd185c00ac44f3fc5c97f
simonescollo <simone.scollo1@gmail.com>
2021-11-16 14:12:45 +0100

    Add project files.

---
+++ ProgettoIndividuale/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProgettoIndividuale.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
